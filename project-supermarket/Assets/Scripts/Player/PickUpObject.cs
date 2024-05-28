16979970f6e0f9d2b560e54ba2edddeedd9b21c0
timpan42 <tim@fagerdal.se>
2024-05-28 14:29:41 +0200

    bones works, trying to have a interaction between hand and object

---
+++ project-supermarket/Assets/Scripts/Player/PickUpObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+
+}
--- project-supermarket/Assets/Scripts/Player/PlayerHand.cs
+++ project-supermarket/Assets/Scripts/Player/PlayerHand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,13 @@ public class PlayerHand : MonoBehaviour
     private InputAction handInputPosition;
     private Vector2 handPosition;
     private Camera camera;
+
+    [SerializeField] private Vector2 handSize;
+    [SerializeField] private Transform handTransform;
+    [SerializeField] private LayerMask layerToHit;
+    private RaycastHit2D[] rayHitArray = new RaycastHit2D[5];
+    private int rayHitAmount;
+
 
     private void Awake()
     {
@@ -29,6 +37,7 @@ public class PlayerHand : MonoBehaviour
     private void Update()
     {
         HandPositionUpdate();
+        RayFindObject();
     }
 
     private void HandPositionUpdate()
@@ -39,5 +48,26 @@ public class PlayerHand : MonoBehaviour
 
         transform.position = camera.ScreenToWorldPoint(new Vector3(xPos, yPos, 1f));
         print(camera.ScreenToWorldPoint(new Vector2(xPos, yPos)));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        // Then use it one a default cube which is not translated nor scaled
+        Gizmos.DrawWireCube(handTransform.position, handSize);
+
+    }
+
+    private void RayFindObject()
+    {
+        rayHitAmount = Physics2D.BoxCastNonAlloc(handTransform.position, handSize, 0, Vector2.zero, rayHitArray);
+
+        if (rayHitAmount > 0)
+        {
+            for (int i = 0; i < rayHitAmount; i++)
+            {
+                Destroy(rayHitArray[i].collider.gameObject);
+            }
+        }
     }
 }
