1f98467293e9b73619e5021206f8f8c886d28dd0
timpan42 <tim@fagerdal.se>
2024-05-29 13:55:28 +0200

    fixade arm och att ta up

--- project-supermarket/Assets/Scripts/Player/PlayerHand.cs
+++ project-supermarket/Assets/Scripts/Player/PlayerHand.cs
@@ -8,12 +8,14 @@ public class PlayerHand : MonoBehaviour
 {
     private PlayerInput playerInput;
     private InputAction handInputPosition;
-    private Vector2 handPosition;
+    private Vector2 mousePosition;
     private Camera camera;
 
-    [SerializeField] private Vector2 handSize;
+    [SerializeField] private Vector2 handDetectorSize;
     [SerializeField] private Transform handTransform;
+    [SerializeField] private Vector3 handCollisionDetectorOffset;
     [SerializeField] private LayerMask layerToHit;
+    private Vector2 handCollisionDetectorOrigin;
     private RaycastHit2D[] rayHitArray = new RaycastHit2D[5];
     private int rayHitAmount;
 
@@ -36,36 +38,35 @@ public class PlayerHand : MonoBehaviour
 
     private void Update()
     {
-        HandPositionUpdate();
+        MousePositionUpdate();
         RayFindObject();
     }
 
-    private void HandPositionUpdate()
+    private void MousePositionUpdate()
     {
-        handPosition = handInputPosition.ReadValue<Vector2>();
-        float xPos = handPosition.x;
-        float yPos = handPosition.y;
+        mousePosition = handInputPosition.ReadValue<Vector2>();
+        float xPos = mousePosition.x;
+        float yPos = mousePosition.y;
 
         transform.position = camera.ScreenToWorldPoint(new Vector3(xPos, yPos, 1f));
-        print(camera.ScreenToWorldPoint(new Vector2(xPos, yPos)));
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        // Then use it one a default cube which is not translated nor scaled
-        Gizmos.DrawWireCube(handTransform.position, handSize);
-
+        Gizmos.DrawWireCube(handCollisionDetectorOrigin, handDetectorSize);
     }
 
     private void RayFindObject()
     {
-        rayHitAmount = Physics2D.BoxCastNonAlloc(handTransform.position, handSize, 0, Vector2.zero, rayHitArray);
+        handCollisionDetectorOrigin = handTransform.position + handCollisionDetectorOffset;
+        rayHitAmount = Physics2D.BoxCastNonAlloc(handCollisionDetectorOrigin, handDetectorSize, 0, Vector2.zero, rayHitArray);
 
         if (rayHitAmount > 0)
         {
             for (int i = 0; i < rayHitAmount; i++)
             {
+                Debug.Log(rayHitArray[i]);
                 Destroy(rayHitArray[i].collider.gameObject);
             }
         }
